62ca5b2d42dee9ce81eade3560a836a22ae19e73
MikeyKanyungo <mikekanyungo95@gmail>
2023-12-11 21:51:49 +0100

    further file reorganisation

---
+++ Project_10_-_wandaArtGenerator/script.js
@@ -0,0 +1,36 @@
+const generate  = document.getElementById("generateBtn");
+const save = document.getElementById("saveBtn");
+
+const shapes = [
+"quad-circle-1", 
+"quad-circle-2", 
+"qaud-circle-3",
+"quad-circle-4",
+
+"triangle-1", 
+"triangle-2", 
+"triangle-3",
+"triangle-4", 
+
+"circle",
+"square",
+"spark",
+"star",
+"rotated-square",
+];
+
+const colors = ["#01d2fd", "#ffc700", "#fe9f12", "#06d0c7", ];
+const boxes = document.querySelectorAll(".container div"); 
+
+let generatePattern = () => {
+    boxes.forEach((box) => {
+        box.className = "";
+        let i = Math.floor(Math.random() * shapes.length);
+        let j = Math.floor(Math.random() * colors.length);
+        box.classList.add(shapes[i]);
+        box.style.backgroundColor = colors[j];
+    });
+};
+
+generate.addEventListener("click", generatePattern);
+window.addEventListener("load", generatePattern);
